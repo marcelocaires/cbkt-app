474bb48df621b6fd3ecc253a79a1e0083fe47bbb
080144070515 <mmcaires@tre-ba.jus.br>
2025-10-17 12:56:47 -0300

    feat: atualizar versão do Angular SSR, refatorar componente de carteirinha de atleta e aprimorar a interface do usuário

--- src/app/project/atleta/components/atleta-carteirinha/atleta-carteirinha.component.ts
+++ src/app/project/atleta/components/atleta-carteirinha/atleta-carteirinha.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { FaixaKarateComponent } from '../../../graduacao/components/faixa-karate';
 import { Atleta } from '../../models/atleta.model';
 
 @Component({
   selector: 'app-atleta-carteirinha',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FaixaKarateComponent],
   templateUrl: './atleta-carteirinha.component.html',
   styleUrls: ['./atleta-carteirinha.component.scss']
 })
--- src/app/project/atleta/models/atleta.model.ts
+++ src/app/project/atleta/models/atleta.model.ts
@@ -1,52 +1,74 @@
 export interface Atleta {
-  id: number;
-  nomeAtleta: string;
-  dataCadastro: string;
-  sexo: string;
-  dataNascimento: string;
-  filiacaoMae: string;
-  filiacaoPai: string;
-  diaAnuidade: number;
-  mesAnuidade: number;
-  categoria: string;
-  codigoGraduacao: number;
-  descricaoGraduacao: string;
-  codigoClube: number;
-  nomeClube: string;
-  codigoClubeInicial: number;
-  chkArbitro: boolean;
-  chkAvaliador: boolean;
-  ativo: boolean;
-  codigoCategoria: number;
-  chkArbitroCategoria: boolean;
-  pcd: boolean;
-  urlFoto: string;
-  dataFaixa: string | null;
-  graduacao: string;
-  nacionalidade: string;
-  naturalidade: string;
-  nomeMae: string;
-  nomePai: string;
-  observacao: string;
-  documentos: {
-    cpf: string;
-    rg: string;
-    rgOrgao: string;
-    rgEstado: string;
-    certidaoNascimento: string;
-  };
-  endereco: {
-    logradouro: string;
-    numero: string;
-    bairro: string;
-    complemento: string;
-    cidade: string;
-    estado: string;
-    cep: string;
-    uf: string;
-  };
-  contato: {
-    email: string;
-    telefone: string;
-  };
+  id: number
+  nomeAtleta: string
+  dataCadastro: string
+  sexo: string
+  dataNascimento: string
+  filiacaoMae: string
+  filiacaoPai: string
+  diaAnuidade: number
+  mesAnuidade: number
+  categoria: string
+  graduacao: Graduacao
+  codigoClube: number
+  nomeClube: string
+  codigoClubeInicial: number
+  chkArbitro: boolean
+  chkAvaliador: boolean
+  ativo: boolean
+  codigoCategoria: number
+  chkArbitroCategoria: boolean
+  pcd: boolean
+  urlFoto: string
+  dataFaixa: any
+  nacionalidade: string
+  naturalidade: string
+  nomeMae: string
+  nomePai: string
+  observacao: string
+  documentos: Documentos
+  endereco: Endereco
+  contato: Contato
+}
+
+export interface Graduacao {
+  id: number
+  descricaoGraduacao: string
+  grau: string
+  grauNome: string
+  cor: any
+  corNome: any
+  corHex: any
+  carencia: number
+  carenciaMenor: number
+  numeroAulas: any
+  valor: number
+  idadeMinima: number
+  anuidadeAte: number
+  anuidadeApos: number
+  anuidade: string
+}
+
+export interface Documentos {
+  cpf: string
+  rg: string
+  rgOrgao: string
+  rgEstado: string
+  certidaoNascimento: string
+}
+
+export interface Endereco {
+  logradouro: string
+  numero: string
+  bairro: string
+  complemento: string
+  cidade: string
+  estado: string
+  cep: string
+  uf: string
+}
+
+export interface Contato {
+  email: string
+  telefone: string
 }
